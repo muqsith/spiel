1dd93f47caa7feb77b3e1b3690f4dfe4dfde2f2a
mui <mui@one.com>
2020-06-28 05:34:56 +0400

    First exercise from Learn OpenGL

--- pilot/src/tests/hello.cpp
+++ pilot/src/tests/hello.cpp
@@ -8,6 +8,19 @@
 
 #include <iostream>
 
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
 int main()
 {
     glfwInit();
@@ -24,11 +37,28 @@ int main()
     }
     glfwMakeContextCurrent(window);
 
+    // registering for callback events go here
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        // rendering commands go here
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // OpenGL's state-setting function
+        glClear(GL_COLOR_BUFFER_BIT);         // OpenGL's state-using function
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 
     return 0;
 }
